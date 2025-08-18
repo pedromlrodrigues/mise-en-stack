384d399babb217e4bb4503551c6d85d9fccf1e98
Pedro Rodrigues (CTW) <pedro.ml.rodrigues@ctw.bmwgroup.com>
2025-08-18 23:24:04 +0100

    chore(ui): initialize react application with vite

---
+++ ui/src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage.jsx';
+import RecipesPage from './pages/RecipesPage/RecipesPage.jsx';
+import Header from './components/Header/Header.jsx';
+
+function App() {
+  return (
+  );
+}
+
+export default App;
