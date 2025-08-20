1cc91b23f58328b03725783726cfa36dcc74a326
Pedro Rodrigues (CTW) <pedro.ml.rodrigues@ctw.bmwgroup.com>
2025-08-20 23:08:27 +0100

    feat(ui): add homepage background image.

--- ui/src/pages/HomePage/HomePage.jsx
+++ ui/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import styles from './HomePage.module.css';
+import backgroundImage from '../../assets/kitchen-tools-bg.png';
 
 function HomePage() {
   const navigate = useNavigate();
@@ -9,11 +10,14 @@ function HomePage() {
     navigate(`/recipes?search=${encodeURIComponent(searchTerm)}&page=1`);
   };
   return (
-    <>
-      <main className={styles.mainContent}>
-        <SearchBar onSubmit={handleSearchSubmit} />
-      </main>
-    </>
+    <main className={styles.mainContent}>
+      <img
+        src={backgroundImage}
+        alt="" // Alt text is empty as the image is purely decorative
+        className={styles.backgroundImage}
+      />
+      <SearchBar onSubmit={handleSearchSubmit} />
+    </main>
   );
 }
 
